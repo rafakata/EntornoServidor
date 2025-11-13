4cd7fee81d1336fa922fa70e4ae6f345f302be71
rafakata <rafa.medina.quelle@gmail.com>
2025-11-13 10:20:27 +0100

    subida de examen.

--- Examen13-11/examen_daw.php
+++ Examen13-11/examen_daw.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Examen Servidor-Rafael Medina Quelle</title>
+    <!-- https://github.com/rafakata/EntornoServidor.git -->
 </head>
 <body>
     
